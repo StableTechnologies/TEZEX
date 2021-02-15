fd693620fe281c8dd482d4fa003c8b9a64cc7678
Soumya Ghosh Dastidar <gdsoumya@gmail.com>
2021-02-15 10:25:09 +0530

    Updated package.json and eslint config
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- bot/.eslintrc.js
+++ bot/.eslintrc.js
@@ -4,9 +4,16 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: "airbnb",
+  rules: {
+    "consistent-return": 2,
+    "comma-dangle": ["error", "never"],
+    indent: [1, 4],
+    "no-else-return": 1,
+    semi: [1, "always"],
+    "space-unary-ops": 2,
+  },
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {},
 };
