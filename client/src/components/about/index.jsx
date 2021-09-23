32e844550806ba195339e74aee3e6b58f8ed9e22
aeesh <adamaisha352@gmail.com>
2021-09-23 07:33:24 +0100

    about page content updated

--- client/src/components/about/index.jsx
+++ client/src/components/about/index.jsx
@@ -12,61 +12,97 @@ import { Typography } from "@material-ui/core";
 const About = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-      {/* <Grid item className = {classes.sidelogoconainer} xs={1}>
-        {" "}
-          <img className = {classes.sidelogo} src={sidelogo} />
-      </Grid> */}
-      <Typography variant="h1" > About Tezex </Typography>
-      <div className={classes.container}>
-        <div className={classes.txtCon}>
-          <Typography variant="h4" >What are Atomic Swaps?</Typography>
-          <Typography> Atomic swaps are automated, self-enforcing crypto-currency exchange contracts
-            that allow crypto-currencies to be traded peer-to-peer without the need for a trusted third party.
-          </Typography>
-        </div>
-
-        <div className={classes.txtCon}>
-          <Typography variant="h4">Why TEZEX?</Typography>
-          <Typography>
-            The goal of atomic swaps was to remove any need for centralized 3rd parties,
-            but most exchanges that provide "Decentralized" Atomic Swaps have some form of centralized backend service
-            which is being used for either order book maintenance, order matching or both.
-          </Typography>
-          <Typography>
-            TEZEX provides complete Decentralized Atomic Swaps, it is not dependent on any centralized third-party service.
-          </Typography>
-          <Typography>
-            All stages of a swap are performed by the client application(this website), hence it is mandatory
-            for the client to be active throughout the swap. If the app is disconnected or closed before a swap is
-            completed or refunded the data will be lost and your swap assets will not be recoverable.
-          </Typography>
-        </div>
-
-        <div className={classes.txtCon}>
-          <Typography variant="h4"> How to Perform a Swap? </Typography>
-          <Typography>
-            Currently, TEZEX provides cross chain atomic swaps between Ethereum and Tezos, to start a swap follow these steps :
-          </Typography>
-          <List>
-              <ListItem>1. Visit the New Swap option from the navigation.</ListItem>
-              <ListItem>2. Select which crypto-currency you want to swap(USDC&#8594;USDtz or USDtz&#8594;USDC).</ListItem>
-              <ListItem>3. In the following screen you can create an new swap by entering the amount you want to exchange.</ListItem>
-              <ListItem>4. Once a swap is generated you can see the Swap state in the Home page.</ListItem>
-          </List>
-        </div>
-
-        <div className={classes.txtCon}>
-          <Typography variant="h4"> The swap can be in the following states : </Typography>
-          <List>
-            <ListItem> 1. Swap Initiated : Swap Request initiated, waiting for response Swaps. </ListItem>
-            <ListItem> 2. Swap Response Found : Swap response has been found and can proceed to completion. </ListItem>
-            <ListItem> 3. Completed : The Swap was successfully completed.</ListItem>
-            <ListItem> 4. Refunded : There was no Response for the Swap, it expired and the funds have been returned.</ListItem>
-          </List>
-        </div>
-      </div>
-    </div>
+    <Grid container justify="center" className={ `${classes.root} ${classes.container}` }>
+      <Grid item xs={9} justify="center">
+        <Typography> About </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography>
+          TezEx (Tezos Exchange) is a next-generation decentralized exchange and decentralized cross-chain bridge
+          built on Tezos blockchain. On TezEx there are no pools, no custody, and no price-slippage. TezEx is powered by
+          a network of decentralized atomic swaps, a ‘peer-to-peer’ process without any central intermediaries.
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Get in touch </Typography>
+        <Grid >
+          <img src="" alt="" />
+        </Grid>
+        <Grid >
+          <img src="" alt="" />
+        </Grid>
+        <Grid >
+          <img src="" alt="" />
+        </Grid>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> How TezEx works </Typography>
+        <Grid>
+          <img src="" alt="" />
+        </Grid>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Changemakers </Typography>
+        <Typography>
+          Liquidity Providers on TezEx are called ‘Changemakers’. Changemakers do not deposit their liquidity into pools or to a central custodian.
+          Rather, they keep their liquidity in their own custody. Just like for traders, funds only move out of a changemaker’s wallet when it’s time to swap.
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Satellites </Typography>
+        <Typography>
+          A changemaker’s setup is called a ‘satellite’. A changemaker’s satellite includes the capital set aside for trading
+          as well as the swap-bots that automate the trading process for them. Changemakers configure their bots to accept desired currencies in exchange for offered currencies.
+          Over time, more customizable features will come to changemaker satellite bots including price tolerances and limit orders.
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> TezEx vs. an AMM (like Uniswap, Quipuswap, PlentyDeFi: Swap, etc.)  </Typography>
+        <Typography>
+          Whereas AMMs (Automated Market Makers) prices for trades are set by an algorithm that reflects previous price action and is subject to slippage,
+          TezEx prices are set by Harbinger oracle. AMM pricing algorithms are a seesaw, moving the price up and down as a consequence of the previous trade.
+          If in an AMM, traders are suddenly trading a lot of their A for B, so they could get more B, then the algorithm reacts by increasing the price of B relative to A for the next trader.
+          If the reverse were to occur and traders started selling B for A so that they could get more A, then the algorithm would respond by increasing the price of A relative to B for the next trader.
+        </Typography>
+        <Typography>
+          Whereas liquidity on an AMM is deposited to a smart contract, TezEx liquidity is non-custodial and only transacts in lock-step with the trader (i.e. the other party)
+          making a swap at the same time. Non-custodial liquidity providing is a hallmark feature of TezEx.
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Prices </Typography>
+        <Typography>
+          Fully-convertible ‘like-assets’ (e.g. USDC-USDtz, USDT-USDtz, ETH-ETHtz, BTC-BTCtz, WBTC-tzBTC) are hardcoded in price 1-1.
+        </Typography>
+        <Typography>
+          Price oracles are used for incongruently priced tokens of a pair (e.g. XTZ-USDtz, ETHtz-USDtz).
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Refunds </Typography>
+        <Typography>
+          If a swap process is interrupted and can not move on to the redemption stage, the smart-contract is released back to the account from which it came.
+          As per the smart-contract, no account can become the recipient of these funds other than the original public key (account; wallet address).
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Changemaker rewards </Typography>
+        <Typography>
+          Changemakers earn rewards on any swap involving their liquidity. The main reward is the percentage fee which can vary from
+          15 basis points (0.15%) to 45 basis points (0.45%), and in rare cases in which the cost of inventory is high 100 basis points (1.00%).
+        </Typography>
+        <Typography>
+          A cross-chain transaction can be gas-intensive, but nonetheless the changemaker must front the gas costs for those transactions.
+          However, the gas cost is deducted from the trader’s final settlement and given to the changemaker; effectively,
+          the trader pays the gas costs for the changemaker. Gas costs are calculated by taking an oracle reading of the gas price for the last block,
+          and adding a cushion to account for block-to-block gas fluctuations.
+        </Typography>
+      </Grid>
+      <Grid item xs={9} text-align="left">
+        <Typography> Audits </Typography>
+        <Typography> TEZEX has been audited by Apriorit. View the report here. [link to pdf] </Typography>
+      </Grid>
+    </Grid>
    );
 };
 
--- client/src/components/about/style.js
+++ client/src/components/about/style.js
@@ -2,57 +2,63 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& .MuiList-root, MuiListItem-gutters': {
+    '& .MuiGrid-item': {
+      padding: '1rem 0',
+    },
+    "& .MuiTypography-root": {
       padding: '.5rem 0',
-    },
-    '& .MuiListItem-gutters': {
-      padding: '.5rem 0',
-    },
-    "& .MuiTypography-h1": {
-      fontSize: "2.5rem",
-      fontWeight: "700",
-      lineHeight: "4.2rem",
-      letterSpacing: "0.1rem",
-      margin: "1rem",
-      textAlign: "center",
-    },
-    '& .MuiTypography-h4': {
-      fontSize: '2rem',
-      fontWeight: '500',
-    },
-    '& .MuiTypography-body1': {
-      padding: '.5rem 0',
-      lineHeight: "1.1rem",
-    },
-  '@media (max-width: 501px)': {
-    '& .MuiTypography-h1': {
-      fontSize: '2rem',
-    },
-    '& .MuiTypography-h4': {
-      fontSize: '1.2rem',
-      fontWeight: '600',
-    },
-  }
+      fontSize: "1rem",
+      lineHeight: "1.1875rem"
+    }
+  //   '& .MuiList-root, MuiListItem-gutters': {
+  //     padding: '.5rem 0',
+  //   },
+  //   '& .MuiListItem-gutters': {
+  //     padding: '.5rem 0',
+  //   },
+  //   "& .MuiTypography-h1": {
+  //     fontSize: "2.5rem",
+  //     fontWeight: "700",
+  //     lineHeight: "4.2rem",
+  //     letterSpacing: "0.1rem",
+  //     margin: "1rem",
+  //     textAlign: "center",
+  //   },
+  //   '& .MuiTypography-h4': {
+  //     fontSize: '2rem',
+  //     fontWeight: '500',
+  //   },
+  //   '& .MuiTypography-body1': {
+  //     padding: '.5rem 0',
+  //     lineHeight: "1.1rem",
+  //   },
+  // '@media (max-width: 501px)': {
+  //   '& .MuiTypography-h1': {
+  //     fontSize: '2rem',
+  //   },
+  //   '& .MuiTypography-h4': {
+  //     fontSize: '1.2rem',
+  //     fontWeight: '600',
+  //   },
+  // }
   },
   container: {
-    width: "70%",
+    textAlign: "justify",
     padding: "1rem",
-    margin: "1rem 1rem 5rem",
-    textAlign: "justify"
   },
-  txtCon: {
-    padding: ".5rem"
-  },
-  '@media (max-width: 769px)': {
-    container: {
-      width: "80%",
-    }
-  },
-  '@media (max-width: 501px)': {
-    container: {
-      width: "85%",
-    }
-  },
+  // txtCon: {
+  //   padding: ".5rem"
+  // },
+  // '@media (max-width: 769px)': {
+  //   container: {
+  //     width: "80%",
+  //   }
+  // },
+  // '@media (max-width: 501px)': {
+  //   container: {
+  //     width: "85%",
+  //   }
+  // },
 }));
 
 export default useStyles;
