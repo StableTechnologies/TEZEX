058396da3a2d6b509f2f85435c234d7fbeabbe70
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-02 16:55:26 +0100

    Broken stock react app, webpack issue with beacon

---
+++ V2/tezex/src/components/session/wallet.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import React, { useEffect } from "react";
+import connectWallet from "../../functions/wallet";
+export const Wallet: FC = () => {
+	const onSubmit = async () => {
+		await connectWallet();
+	};
+	return <>
+	<button onClick={onSubmit}></button>;
+</>;
+};
