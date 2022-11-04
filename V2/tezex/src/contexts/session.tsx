73af3d5bb8acf3ea1e37d82df6a649fbe50fec70
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-04 11:08:05 +0100

    Added a wallet context which will hold the client

--- V2/tezex/src/contexts/session.tsx
+++ V2/tezex/src/contexts/session.tsx
@@ -1,16 +1,21 @@
 import { createContext, useState } from "react";
+import { WalletContext, WalletInfo } from './wallet';
+export const SessionContext = createContext<any | null>(null);
 
-export const SessionContext = createContext();
 
 export function SessionProvider({ children }) {
 	const [isWalletConnected, setIsWalletConnected] = useState(false);
-	return <SessionContext.Provider 
-		value={{
-			isWalletConnected,
-			setIsWalletConnected
-		}}
-	>
-       {children}
-       </SessionContext.Provider>
-
+	const [walletInfo, setWalletInfo] = useState<WalletInfo | null>(null);
+	return (
+		<SessionContext.Provider
+			value={{
+				isWalletConnected,
+				setIsWalletConnected,
+			}}
+		>
+			<WalletContext.Provider value={walletInfo}>
+				{children}
+			</WalletContext.Provider>
+		</SessionContext.Provider>
+	);
 }
---
+++ V2/tezex/src/contexts/wallet.tsx
@@ -0,0 +1,8 @@
+import { createContext, useState } from "react";
+
+export interface WalletInfo {
+	client: any
+	setClient: any
+}
+export const WalletContext = createContext<WalletInfo | null>(null);
+
