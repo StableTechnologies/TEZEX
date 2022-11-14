7db650f1091faeaffa275bb6ec9514dfcbc01334
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-14 10:35:33 +0100

    ISession Interface

--- V2/tezex/src/contexts/session.tsx
+++ V2/tezex/src/contexts/session.tsx
@@ -2,7 +2,16 @@ import { createContext, FC, useEffect, useState } from "react";
 import { WalletContext, WalletInfo } from "./wallet";
 export const SessionContext = createContext<any | null>(null);
 
-export function SessionProvider({ children }) {
+export interface ISession {
+  children:
+    | JSX.Element[]
+    | JSX.Element
+    | React.ReactElement
+    | React.ReactElement[]
+    | string
+}
+
+export function SessionProvider(props: ISession) {
 	const [isWalletConnected, setIsWalletConnected] = useState(false);
 	const [client, setClient] = useState<any | null>(null);
 	const [address, setAddress] = useState<any | null>(null);
@@ -33,7 +42,7 @@ export function SessionProvider({ children }) {
 			}}
 		>
 			<WalletContext.Provider value={walletInfo}>
-				{children}
+				{props.children}
 			</WalletContext.Provider>
 		</SessionContext.Provider>
 	);
