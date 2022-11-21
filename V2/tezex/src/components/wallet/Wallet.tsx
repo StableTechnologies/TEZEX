142a041ff6cf716489712ac8fb66fde3f6ea1991
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-21 14:22:28 +0100

    Swapped beacon with taquito's beacon

--- V2/tezex/src/components/wallet/Wallet.tsx
+++ V2/tezex/src/components/wallet/Wallet.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 import connectWallet from "../../functions/beacon";
 import { useWallet } from "../../hooks/wallet";
+import { useNetwork } from "../../hooks/network";
 import { WalletInfo } from "../../contexts/wallet";
 import { WalletConnected } from "../session/WalletConnected";
 import { WalletDisconnected } from "../session/WalletDisconnected";
 
 export const Wallet: FC = () => {
 	const walletInfo: WalletInfo | null = useWallet();
+	const networkInfo = useNetwork();
 	const connect = async () => {
 		if (walletInfo) { 
-			await connectWallet(walletInfo) ;
+			await connectWallet(walletInfo, networkInfo) ;
 		}
 	};
 	const disconnect = async () => {
