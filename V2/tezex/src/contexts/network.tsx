f0e1664cdae0a00cdd0e543eed279c6a3256acc6
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-21 13:26:58 +0100

    Network context

---
+++ V2/tezex/src/contexts/network.tsx
@@ -0,0 +1,19 @@
+import { createContext} from "react";
+import { NetworkType } from "@airgap/beacon-sdk";
+import { DAppClient } from '@airgap/beacon-sdk';
+
+export interface NetworkInfo {
+    network: NetworkType, 
+    tezosServer: string,
+    tezosServerForTaquito: string,
+    apiKey: string,
+}
+ 
+export const WalletContext = createContext<NetworkInfo>({
+    network: NetworkType.MAINNET,
+    tezosServer: "https://tezos-prod.cryptonomic-infra.tech:443",
+    tezosServerForTaquito: "",
+    apiKey: "ab682065-864a-4f11-bc77-0ef4e9493fa1",
+})
+
+
