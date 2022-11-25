67cd152af02e51ba7d155d240a2353f49366e0c1
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-25 13:23:39 +0100

    WIP Minimal ui to swap token

--- V2/tezex/src/App.tsx
+++ V2/tezex/src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import './App.css';
 
 import { Layout } from "./components/ui/views/Layout";
+import { SwapToken } from "./components/liquidityBaking/swapToken/SwapToken";
 
 export const App: FC = () => {
   return (
@@ -9,7 +10,7 @@ export const App: FC = () => {
       <header className="App-header">
         <p>
 		<Layout>
-			{''}
+			<SwapToken />	
 		</Layout>
         </p>
       </header>
---
+++ V2/tezex/src/components/liquidityBaking/swapToken/SwapToken.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+export interface ISwapToken {
+	children: null;
+}
+export const SwapToken: FC= (props) => {
+	return (
+		<div>
+			<h1>{"Buy Token"}</h1>
+		</div>
+	);
+};
