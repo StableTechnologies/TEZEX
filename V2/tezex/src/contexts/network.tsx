796a5ae637357dfa100220b83c82bc53dfff44f4
Siddharth Vaderaa <siddharth@debian.pro>
2022-11-29 11:19:22 +0100

    Network and addresses

--- V2/tezex/src/contexts/network.tsx
+++ V2/tezex/src/contexts/network.tsx
@@ -1,20 +1,38 @@
-import { createContext} from "react";
+import { createContext } from "react";
 import { NetworkType } from "@airgap/beacon-sdk";
 
+export interface Addresses {
+	tzbtc : { dex : { sirius: string}}
+}
 export interface NetworkInfo {
-    network: NetworkType, 
-    tezosServer: string,
-    tezosServerForTaquito: string,
-    apiKey: string,
-}
- 
-export const networks = {
-    network: NetworkType.MAINNET,
-    tezosServer: "https://tezos-prod.cryptonomic-infra.tech:443",
-    tezosServerForTaquito: "",
-    apiKey: "ab682065-864a-4f11-bc77-0ef4e9493fa1",
+	network: NetworkType;
+	tezosServer: string;
+	tezosServerForTaquito: string;
+	apiKey: string;
+	addresses: Addresses
 }
 
-export const NetworkContext = createContext<NetworkInfo>(networks)
+export type NetworkMap = {
+	[network: string]: NetworkInfo;
+};
+export interface INetwork {
+	selectedNetwork: NetworkType;
+	networks: NetworkMap;
+}
 
+export const mainnet = {
+	network: NetworkType.MAINNET,
+	tezosServer: "https://tezos-prod.cryptonomic-infra.tech:443",
+	tezosServerForTaquito: "",
+	apiKey: "ab682065-864a-4f11-bc77-0ef4e9493fa1",
+	addresses: {tzbtc : { dex : { sirius: ""}}} //TODO
+};
 
+const networks: NetworkMap = {
+	[NetworkType.MAINNET as string]: mainnet,
+};
+
+export const NetworkContext = createContext<INetwork>({
+	selectedNetwork: NetworkType.MAINNET,
+	networks,
+});
