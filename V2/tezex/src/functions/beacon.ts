130dcd47edbaac8b3d376ff41a0bf1d42b5d8351
Siddharth Vaderaa <siddharth@debian.pro>
2022-12-05 10:47:17 +0100

    Balance check WIP

--- V2/tezex/src/functions/beacon.ts
+++ V2/tezex/src/functions/beacon.ts
@@ -2,11 +2,46 @@ import { DAppClient } from "@airgap/beacon-sdk";
 import { WalletInfo } from "../contexts/wallet";
 import { NetworkInfo } from "../contexts/network";
 import { BeaconWallet } from "@taquito/beacon-wallet";
-import { TezosToolkit, MichelCodecPacker  } from "@taquito/taquito";
-  
-
+import { TezosToolkit, MichelCodecPacker } from "@taquito/taquito";
+import {BigNumber} from 'bignumber.js';
 import { NetworkType } from "@airgap/beacon-sdk";
 import { useNetwork } from "../hooks/network";
+
+import { TokenKind } from "../types/general";
+
+export function mutezToTez(amount: BigNumber) {
+	return amount.dividedBy(1000000)
+}
+export async function getBalance(
+	walletInfo: WalletInfo,
+	asset: TokenKind,
+	mantissa: boolean
+) {
+	if (walletInfo.toolkit && walletInfo.address) {
+		switch (asset) {
+			case TokenKind.XTZ:
+				const amount =
+					await walletInfo.toolkit.tz.getBalance(
+						walletInfo.address
+					);
+				if (!mantissa) {
+					return mutezToTez(amount);
+				} else {
+					return amount;
+				}
+			case TokenKind.TzBTC: return new BigNumber(0); //todo
+		}
+	} else return new BigNumber(0);
+}
+export async function hasSufficientBalance(
+	minimumBalance: BigNumber,
+	walletInfo: WalletInfo,
+	asset: TokenKind,
+	mantissa: boolean
+): Promise<boolean>{
+	const balance = await getBalance(walletInfo, asset, mantissa);
+	return minimumBalance.isLessThanOrEqualTo(balance);
+}
 
 export default async function connectWallet(
 	walletInfo: WalletInfo,
@@ -17,34 +52,33 @@ export default async function connectWallet(
 		preferredNetwork: network.network,
 	});
 
-
-  const tezos = new TezosToolkit(network.tezosServer);
+	const tezos = new TezosToolkit(network.tezosServer);
 	var err = false;
 	try {
-		await beaconWallet.requestPermissions({network: {type: network.network}});
+		await beaconWallet.requestPermissions({
+			network: { type: network.network },
+		});
 		const activeAccount =
 			await beaconWallet.client.getActiveAccount();
 
-
-  tezos.setPackerProvider(new MichelCodecPacker());
-  tezos.setWalletProvider(beaconWallet);
+		tezos.setPackerProvider(new MichelCodecPacker());
+		tezos.setWalletProvider(beaconWallet);
 		if (!activeAccount) {
 			throw new Error("Could not connect");
 		} else {
-		walletInfo.setAddress(activeAccount.address);
+			walletInfo.setAddress(activeAccount.address);
 		}
 
-// signer provider ??
+		// signer provider ??
 	} catch (error) {
 		err = true;
 	} finally {
-		if(err){
+		if (err) {
 			walletInfo.setClient(null);
-	         walletInfo.setToolkit(null);
+			walletInfo.setToolkit(null);
 		} else {
-
-		 walletInfo.setClient(beaconWallet.client);
-		 walletInfo.setToolkit(tezos);
+			walletInfo.setClient(beaconWallet.client);
+			walletInfo.setToolkit(tezos);
 		}
 	}
 }
--- V2/tezex/src/types/general.ts
+++ V2/tezex/src/types/general.ts
@@ -1,31 +1,8 @@
 import { KeyStore, KeyStoreType } from 'conseiljs';
 
-export interface TokenTransaction {
-    amount: number;
-    block_level: string;
-    destination: string;
-    fee: number;
-    kind: string;
-    status: string;
-    source: string;
-    timestamp: number;
-    parameters: string;
-    operation_group_hash: string;
-    entryPoint?: string;
-}
-
 export enum TokenKind {
-    tzip7 = 'tzip7',
-    usdtz = 'usdtz',
-    tzbtc = 'tzbtc',
-    wxtz = 'wxtz',
-    ethtz = 'ethtz',
-    kusd = 'kusd',
-    blnd = 'blnd',
-    objkt = 'objkt',
-    stkr = 'stkr',
-    tzip12 = 'tzip12',
-    plenty = 'plenty',
+  XTZ = "XTZ",
+  TzBTC = "TzBTC",
 }
 
 export interface TokenDefinition {
@@ -41,57 +18,5 @@ export interface TokenDefinition {
     tokenIndex?: number; // FA2/tzip12 tokens have an index, frequently 0
     balancePath?: string; // JSON path to the element inside the ledger bigmap value
     helpLink?: string;
-}
-
-export interface Token extends TokenDefinition {
-    balance: number;
-    transactions: TokenTransaction[];
-    activeTab?: string;
-    details?: any;
-    precision?: number;
-    round?: number;
-    transactionFeeFloor?: number;
-    displayHelpLink?: string;
-    hideOnLanding?: boolean;
-}
-
-/**
- * A special token which is generated through locking XTZ in "vaults"
- */
-export interface VaultToken extends Token {
-    // Address of a contract which can originate Ovens
-    vaultCoreAddress: string;
-
-    // ID of a BigMap that contains the Oven Registry
-    vaultRegistryMapId: number;
-
-    // A list of Ovens owned by the user.
-    vaultList: Vault[];
-}
-
-export interface ArtToken extends Token {
-    marketAddress: string;
-    nftMetadataMap: number;
-    holderLocation?: 'key' | 'value'; // used for parsing map content for address of the holder, either in key or in the value
-    provider?: string;
-}
-
-/**
- * Data about an Vault.
- */
-export interface Vault {
-    // TODO(keefertaylor): rename these vars
-
-    /** Contract address of the Oven contract. */
-    ovenAddress: string;
-
-    /** Account of the Oven's owner. */
-    ovenOwner: string;
-
-    /** Balance of the Oven, in Mutez. */
-    ovenBalance: number;
-
-    /** Baker for the oven. */
-    baker: string | undefined;
 }
 
