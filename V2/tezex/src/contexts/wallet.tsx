0684e5a126b9506ba56513220b70bb453412bdcd
Siddharth Vaderaa <siddharth@debian.pro>
2022-12-06 10:28:14 +0100

    Wallet Status WIP

--- V2/tezex/src/contexts/wallet.tsx
+++ V2/tezex/src/contexts/wallet.tsx
@@ -1,26 +1,42 @@
-import { createContext} from "react";
-import { DAppClient } from '@airgap/beacon-sdk';
+import { createContext } from "react";
+import { DAppClient } from "@airgap/beacon-sdk";
 import { TezosToolkit } from "@taquito/taquito";
 
 export enum WalletStatus {
-  DISCONNECTED = "disconnected",
-  READY = "ready",
-  BUSY = "BUSY",
+	DISCONNECTED = "disconnected",
+	READY = "ready",
+	BUSY = "BUSY",
+}
+
+export function walletUser(
+	setWalletStatus: React.Dispatch<React.SetStateAction<WalletStatus>>
+) {
+	const useWallet = async (op: () => Promise<unknown>) => {
+		const setBusy = async () => {
+			setWalletStatus(WalletStatus.BUSY);
+		};
+		const setReady = async () => {
+			setWalletStatus(WalletStatus.READY);
+		};
+		await setBusy();
+		await op();
+		await setReady();
+	};
+
+	return useWallet;
 }
 
 export interface WalletInfo {
-	client: DAppClient | null,
-	setClient: React.Dispatch<React.SetStateAction<DAppClient | null>>, 	
-	toolkit: TezosToolkit | null,
-	setToolkit: React.Dispatch<React.SetStateAction<TezosToolkit | null>>, 	
-	address: string | null,
-	setAddress: React.Dispatch<React.SetStateAction<string | null>>,
-	walletStatus: WalletStatus,
-	setWalletStatus: React.Dispatch<React.SetStateAction<WalletStatus>>,
-	disconnect: () => void
+	client: DAppClient | null;
+	setClient: React.Dispatch<React.SetStateAction<DAppClient | null>>;
+	toolkit: TezosToolkit | null;
+	setToolkit: React.Dispatch<React.SetStateAction<TezosToolkit | null>>;
+	address: string | null;
+	setAddress: React.Dispatch<React.SetStateAction<string | null>>;
+	walletStatus: WalletStatus;
+	setWalletStatus: React.Dispatch<React.SetStateAction<WalletStatus>>;
+	walletUser: (op: () => Promise<unknown>) => Promise<void>;
+	disconnect: () => void;
 }
- 
 
-export const WalletContext = createContext<WalletInfo | null>(null)
-
-
+export const WalletContext = createContext<WalletInfo | null>(null);
