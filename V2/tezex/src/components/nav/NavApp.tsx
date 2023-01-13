a158fcd64c876f4770fd930503f82b553d12ce94
Siddharth Vaderaa <siddharth@debian.pro>
2023-01-13 23:14:15 +0100

    Nav WIP

--- V2/tezex/src/components/nav/NavApp.tsx
+++ V2/tezex/src/components/nav/NavApp.tsx
@@ -3,42 +3,29 @@ import { FC } from "react";
 import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-
 export interface INav {
 	children: string;
 }
+
+const style1 = {
+	root: {
+		backgroundColor: "red",
+	},
+	right: {
+		justifyContent: "center",
+                alignContent: "center"
+	},
+	nav: {
+		display: "flex",
+		fontSize: "1.5vw",
+		justifyContent: "center",
+	},
+};
+
 export const NavApp: FC = () => {
 	const navigate = useNavigate();
 	return (
-		<div className="nav">
-			<Grid container>
-				<Grid
-					data-aos="flip-left"
-					container
-					item
-					xs={6}
-					sm={3}
-					md={2}
-					alignContent="center"
-				></Grid>
-				<Grid
-					container
-					item
-					alignContent="center"
-					xs={6}
-					sm={5}
-					md={6}
-					lg={6}
-				>
-					<Grid item md={2} lg={4}></Grid>
-					<Grid
-						container
-						item
-						alignContent="center"
-						xs={12}
-						md={9}
-						lg={8}
-					>
+		<div flex-direction="row" style={style1.nav}>
 						<Grid
 							item
 							lg={3}
@@ -86,10 +73,7 @@ export const NavApp: FC = () => {
 							>
 								About
 							</Button>
-						</Grid>
 					</Grid>
-				</Grid>
-			</Grid>
-		</div>
+			</div>
 	);
 };
--- V2/tezex/src/components/ui/views/Header.tsx
+++ V2/tezex/src/components/ui/views/Header.tsx
@@ -2,20 +2,47 @@ import { FC } from "react";
 import { AppTitle } from "../elements/Headings";
 import { Wallet } from "../../wallet/Wallet";
 import { NavApp } from "../../nav";
+import Grid from "@mui/material/Grid";
+import logo from "../../../assets/TezexLogo.svg";
+
+const style = {
+	root: {
+		backgroundColor: "red",
+	},
+	right: {
+		justifyContent: "center",
+                alignContent: "center"
+	},
+	header: {
+		display: "flex",
+		fontSize: "1.5vw",
+		justifyContent: "center",
+		
+	},
+};
 
 export const Header: FC = () => {
 	return (
-		<div>
-			<header>
-				<div>
-					<AppTitle>TEZEX</AppTitle>
-				</div>
+			<div flex-direction="row" style={style.header}>
+				<Grid
+					data-aos="flip-left"
+					container
+					item
+					xs={6}
+					sm={3}
+					md={2}
+					alignContent="center"
+				>
+					<img src={logo} alt="Logo" />
+				</Grid>
+
 				<NavApp />
-				<div>
-					<Wallet />
-				</div>
-			</header>
-		</div>
+
+                    <Grid container item xs={12} sm={4} md={4} lg={4} style={style.right}>
+
+				<Wallet />
+		</Grid>	
+	</div>
 	);
 };
 export {};
