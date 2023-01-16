7d13e241f1b2f8eab2c07a0cc6a0baa641ca2f63
Siddharth Vaderaa <siddharth@debian.pro>
2023-01-16 18:12:35 +0100

    Mui imports

--- V2/tezex/src/pages/Home/index.tsx
+++ V2/tezex/src/pages/Home/index.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import {NavHome} from '../../components/nav'
+import {Swap} from '../../components/Swap'
 import { AddLiquidity  } from "../addLiquidity";
 import { RemoveLiquidity  } from "../removeLiquidity";
-import { Swap } from "../swap";
 
 type HomePaths = 'swap' | 'add' | 'remove'
 
