ac4ea72f06c249ea3f698b856f8cbf3d1427d064
Siddharth Vaderaa <siddharth@debian.pro>
2023-02-16 12:23:07 +0100

    Home cleanup

--- V2/tezex/src/pages/Home/index.tsx
+++ V2/tezex/src/pages/Home/index.tsx
@@ -1,50 +1,49 @@
 import { FC } from "react";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import {NavHome} from '../../components/nav'
-import {Swap} from '../../components/swap'
-import { AddLiquidity  } from "../../components/addLiquidity";
-import { RemoveLiquidity  } from "../../components/removeLiquidity";
+import { NavHome } from "../../components/nav";
+import { Swap } from "../../components/swap";
+import { AddLiquidity } from "../../components/addLiquidity";
+import { RemoveLiquidity } from "../../components/removeLiquidity";
 
-import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from '@mui/material/CardHeader';
-import Grid from "@mui/material/Grid";
-//import KeyboardArrowDownIcon from '@mui/material/icons/KeyboardArrowDown';
-import Paper from '@mui/material/Paper';
-import TextField from '@mui/material/TextField';
-import Typography from "@mui/material/Typography";
-
-type HomePaths = 'swap' | 'add' | 'remove'
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
+type HomePaths = "swap" | "add" | "remove";
 
 export interface IHome {
-	path: HomePaths
+	path: HomePaths;
 }
 export const Home: FC<IHome> = (props) => {
 	const c = props.path;
 
-	const Comp = (() =>{ switch(props.path) {
-		case 'add': 
-			return AddLiquidity;
-		case 'remove': 
-			return RemoveLiquidity;
-		case 'swap': 
-			return Swap;
-	}})();
+	const Comp = (() => {
+		switch (props.path) {
+			case "add":
+				return AddLiquidity;
+			case "remove":
+				return RemoveLiquidity;
+			case "swap":
+				return Swap;
+		}
+	})();
 
 	return (
-		<Grid2 sx={{ flexDirection: "column", justifyContent: "center"}} container>
-<Grid2  sx={{paddingBottom: "32px" ,  paddingTop: "56px"}}>
-
-		           <NavHome /> 	
-</Grid2>
-<Grid2>
-
+		<Grid2
+			sx={{
+				flexDirection: "column",
+				justifyContent: "center",
+			}}
+			container
+		>
+			<Grid2
+				sx={{
+					paddingBottom: "32px",
+					paddingTop: "56px",
+				}}
+			>
+				<NavHome />
+			</Grid2>
+			<Grid2>
 				<Comp />
-</Grid2>
-</Grid2>
+			</Grid2>
+		</Grid2>
 	);
 };
