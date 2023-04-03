c0be025a5a15a2db97261b75c45aaa82d3b8e16f
Siddharth Vaderaa <svaderaa@gmail.com>
2023-04-03 11:58:50 +0530

    SwapIcon useStyles

--- V2/tezex/src/components/ui/elements/Toggles.tsx
+++ V2/tezex/src/components/ui/elements/Toggles.tsx
@@ -3,43 +3,20 @@ import Button from "@mui/material/Button";
 import swapIcon from "../../../assets/swapIcon.svg";
 
 import Box from "@mui/material/Box";
+
+import style from "./style";
+import useStyles from "../../../hooks/styles";
+
 export interface IToggle {
   toggle: () => void;
 }
 
 export const SwapUpDownToggle: FC<IToggle> = (props) => {
+  const styles = useStyles(style);
   return (
-    <Button
-      sx={{
-        minWidth: "2.4vw",
-        minHeight: "2.4vw",
-
-        maxWidth: "2.4vw",
-        maxHeight: "2.4vw",
-        boxShadow: "0px 0.28vw 1.38vw rgba(181, 181, 181, 0.25)",
-        background: "#FFFFFF",
-        borderRadius: ".55vw",
-      }}
-      onClick={props.toggle}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-
-          minWidth: "2.2vw",
-          minHeight: "2.2vw",
-        }}
-      >
-        <img
-          style={{
-            padding: ".2vw",
-            width: "100%",
-            height: "100%",
-          }}
-          src={swapIcon}
-          alt="swapIcon"
-        />
+    <Button sx={styles.button} onClick={props.toggle}>
+      <Box sx={styles.box}>
+        <img style={styles.icon} src={swapIcon} alt="swapIcon" />
       </Box>
     </Button>
   );
