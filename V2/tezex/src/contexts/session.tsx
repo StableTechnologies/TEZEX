19d0387d24ba8ad4bfc9d5ed99e362cde844bef4
Siddharth Vaderaa <svaderaa@gmail.com>
2023-04-10 17:46:13 +0530

    Alert added to session

--- V2/tezex/src/contexts/session.tsx
+++ V2/tezex/src/contexts/session.tsx
@@ -1,8 +1,8 @@
 import React, { createContext, useCallback, useState } from "react";
 import { WalletProvider } from "./wallet";
 import { NetworkContext, networkDefaults } from "./network";
-
-import { TransactingComponent } from "../types/general";
+import { Alert } from "../components/ui/elements/dialogs/Alerts";
+import { TransactingComponent, CompletionRecord } from "../types/general";
 
 export const SessionContext = createContext<SessionInfo>({
   loadComponent: (_: TransactingComponent) => {
@@ -10,11 +10,16 @@ export const SessionContext = createContext<SessionInfo>({
     null;
   },
   activeComponent: null,
+  setAlert: (_: CompletionRecord) => {
+    _;
+    null;
+  },
 });
 
 export interface SessionInfo {
   loadComponent: (comp: TransactingComponent) => void;
   activeComponent: TransactingComponent | null;
+  setAlert: (record: CompletionRecord) => void;
 }
 export interface ISession {
   children:
@@ -29,6 +34,16 @@ export function SessionProvider(props: ISession) {
   const [activeComponent, setActiveComponent] =
     useState<TransactingComponent | null>(null);
 
+  const [alerts, setAlerts] = useState<CompletionRecord | undefined>(undefined);
+
+  const clearAlert = useCallback(() => {
+    setAlerts(undefined);
+  }, []);
+
+  const setAlert = useCallback((record: CompletionRecord) => {
+    setAlert(record);
+  }, []);
+
   const loadComponent = useCallback((comp: TransactingComponent) => {
     setActiveComponent(comp);
   }, []);
@@ -38,10 +53,12 @@ export function SessionProvider(props: ISession) {
       value={{
         loadComponent,
         activeComponent,
+        setAlert,
       }}
     >
       <NetworkContext.Provider value={networkDefaults}>
         <WalletProvider>{props.children}</WalletProvider>
+        <Alert completionRecord={alerts} toggle={clearAlert} />
       </NetworkContext.Provider>
     </SessionContext.Provider>
   );
