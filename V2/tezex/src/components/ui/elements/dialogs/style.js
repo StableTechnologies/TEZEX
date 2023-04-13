ea6117d1c7289bc34b6ebaa2abb7250a8e8d0f9e
Siddharth Vaderaa <svaderaa@gmail.com>
2023-04-13 10:31:57 +0530

    Error styling

--- V2/tezex/src/components/ui/elements/dialogs/style.js
+++ V2/tezex/src/components/ui/elements/dialogs/style.js
@@ -16,23 +16,21 @@ const style = (theme) => {
         width: "29.51vw",
         minHeight: "21.04vw",
         minWidth: "29.51vw",
-        //top: "19vw",
+
         maxHeight: "21.04vw",
         maxWidth: "29.51vw",
         padding: "0px 0px 0px 0px",
         margin: "0px",
       },
-      "& .MuiDialog-root": {
-        height: "100%",
-        width: "29.51vw",
-        padding: "0px 0px 0px 0px",
-        margin: "0px",
+      "& .MuiDialog-container": {
+        alignItems: "flex-start",
       },
 
       top: "19vw",
     },
     dialogContent: {
       "&.MuiDialogContent-root": {
+        zindex: "3",
         alignItems: "center",
         justifyContent: "center",
         padding: "0 0 0 0",
@@ -52,8 +50,8 @@ const style = (theme) => {
     errorText: {
       "&.MuiTypography-root": {
         display: "flex",
-        //t right bot left
-        padding: "1vw 6.3vw 3.19vw 5.3vw ",
+
+        padding: ".5vw 6.3vw 2.6vw 5.3vw ",
         fontFamily: "Inter",
         fontSize: " 1.11vw",
         fontWeight: "400vw",
@@ -63,16 +61,14 @@ const style = (theme) => {
       },
     },
     titleBox: {
-      // padding: "1.9vw 0 0 2.5vw",
-      // paddingLeft: "3.2vw",
-      //paddingRight: "1.9vw",
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center",
       width: "100%",
 
       "& .MuiTypography-root": {
-        padding: "1.9vw 2vw",
+        color: "#000000",
+        padding: "1.9vw 2vw 1.1vw 2vw",
         fontFamily: "Inter",
         fontSize: " 1.66vw",
         fontWeight: "500",
@@ -80,14 +76,9 @@ const style = (theme) => {
       },
     },
     title: {
-      //minWidth: "100%",
       justifyContent: "space-between",
       display: "flex",
       flexDirection: "row",
-      // paddingLeft: "2.2vw", // 0 0 2.5vw",
-      //:paddingRight: "1.9vw", // 0 0 2.5vw",
-
-      //	    padding: "0 0 0 0",
     },
     titleTypography: {
       padding: "0px 0px 0px 0px",
@@ -107,14 +98,12 @@ const style = (theme) => {
     alertIcon: {
       height: " 2.55vw",
 
-      display: "flex",
-
       alignItems: "center",
       width: " 2.86vw",
 
       left: " .37vw",
 
-      //top: " .60vw",
+      top: " .60vw",
     },
 
     button: {
@@ -122,13 +111,7 @@ const style = (theme) => {
 
       width: "8.54vw",
 
-      // left  :"10.48vw",
-
-      // top  :"16.11vw",
-
       borderRadius: ".55vw",
-
-      //padding  :".69vw 1.11vw  .69vw 1.11vw",
 
       "&.MuiButton-root.Mui-disabled": {
         color: "white",
@@ -136,7 +119,6 @@ const style = (theme) => {
       background: "#1E1E1E",
       color: "white",
 
-      //border: "1px solid black",
       fontWeight: "500",
       fontSize: "1.11vw",
       lineHeight: "1.34vw",
