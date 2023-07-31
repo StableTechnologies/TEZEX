c704ffe46c2ab11b0f9bb81ccf44cdcaef4bdbb6
Siddharth Vaderaa <svaderaa@gmail.com>
2023-07-31 11:55:31 +0200

    Optional client for gitbook fetchPage

--- V2/tezex/src/functions/gitbook.ts
+++ V2/tezex/src/functions/gitbook.ts
@@ -1,6 +1,6 @@
 import { GitBookAPI } from "@gitbook/api";
 
-export const getClient = (): GitBookAPI | null => {
+export const getClient = (): GitBookAPI => {
   try {
     const accessToken = process.env.REACT_APP_GITBOOK_ACCESS_TOKEN;
     const client = new GitBookAPI({
@@ -9,16 +9,17 @@ export const getClient = (): GitBookAPI | null => {
     return client;
   } catch (error) {
     console.error("Error initializing GitBook client:", error);
-    return null;
+    throw error;
   }
 };
 export const fetchPage = async (
-  client: GitBookAPI,
   spaceId: string,
-  pageId: string
+  pageId: string,
+  client?: GitBookAPI
 ) => {
   try {
-    const page = await client.spaces.getPageById(spaceId, pageId);
+    const _client = client ? client : getClient();
+    const page = await _client.spaces.getPageById(spaceId, pageId);
     return page;
   } catch (error) {
     console.error("Error fetching GitBook page :", error);
