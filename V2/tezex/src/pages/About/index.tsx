4d9e718740218c4fbeedf48f66fd0c1722559eb4
Siddharth Vaderaa <svaderaa@gmail.com>
2023-08-01 14:47:44 +0200

    About page that redirects

--- V2/tezex/src/pages/About/index.tsx
+++ V2/tezex/src/pages/About/index.tsx
@@ -5,16 +5,18 @@ import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
 import style from "./style";
 import useStyles from "../../hooks/styles";
 
-export const About: FC = (props) => {
+import { useSession } from "../../hooks/session";
+export const About: FC = () => {
   const styles = useStyles(style);
+  const appConfig = useSession().appConfig;
 
   useEffect(() => {
     // Redirect logic here
-    window.location.href = "https://docs.tezex.io/about";
+    window.location.href = appConfig.aboutRedirectUrl; //"https://docs.tezex.io/about";
   }, []);
 
   return (
-    <Grid2 sx={styles.homeContainer} container>
+    <Grid2 sx={styles.aboutContainer} container>
       <Grid2>redirecting....</Grid2>
     </Grid2>
   );
--- V2/tezex/src/pages/About/style.js
+++ V2/tezex/src/pages/About/style.js
@@ -1,19 +1,7 @@
 // eslint-disable-next-line
 const style = (theme) => {
   return {
-    nav: {
-      "&.MuiGrid2-root": {
-        position: "relative",
-        marginTop: "3.89vw",
-        marginBottom: "2.22vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "0px 0px 0px 0px ",
-      },
-    },
-
-    homeContainer: {
+    aboutContainer: {
       display: "flex",
       flexDirection: "column",
       justifyContent: "center",
