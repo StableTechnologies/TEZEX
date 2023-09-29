f40ca1a3077bf51c522442c561a63604988a6e5d
Siddharth Vaderaa <svaderaa@gmail.com>
2023-09-29 10:00:08 +0530

    Move main-window style to style.js

--- V2/tezex/src/components/ui/views/main-window/style.js
+++ V2/tezex/src/components/ui/views/main-window/style.js
@@ -1,78 +1,34 @@
 // eslint-disable-next-line
 const style = (theme) => {
   return {
-    header: {
-      headerBox: {
-        fontSize: "1.5vw",
-        display: "flex",
-        alignItems: "center",
-        width: "100%",
-        height: "5vw",
-        left: "0px",
-        top: "0px",
-        background: "#FFFFFF",
-        marginBottom: ".28vw",
-        boxShadow: ".28vw .28vw .28vw rgba(204, 204, 204, 0.25)",
-        [theme.breakpoints.down("sm")]: {
-          boxShadow: "0px 0px 0px 0px",
-        },
-      },
-
-      logo: {
-        position: "relative",
-        [theme.breakpoints.up("md")]: {
-          width: "11.35vw",
-          marginLeft: "11.39vw",
-          top: ".17vw",
-          boxShadow: "0px 0px 0px 0px",
-        },
-        [theme.breakpoints.down("sm")]: {},
-      },
-
-      nav: {
-        [theme.breakpoints.down("md")]: {
-          display: "none",
-        },
-
-        alignContent: "center",
-      },
-
-      wallet: {
-        display: { xs: "none", sm: "none", md: "flex" },
-        position: "absolute",
-        right: "0px",
-        //left: "51vw",
-        alignContent: "end",
-        justifyContent: "flex-end",
-      },
-    },
-    layout: {
-      layoutBox: {
-        height: "100vh",
-        width: "100%",
-        alignContent: "flex-start",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        display: "flex",
-      },
-      layoutMainwindow: {},
-    },
-
-    mainWindowBackground: {
-      position: "absolute",
-      zindex: "-1",
-      top: "6.9vw",
-      display: "flex",
-      maxWidth: "1vw",
-      minWidth: "1vw",
-      maxHeight: "51.66vw",
-      minHeight: "51.66vw",
-      marginLeft: "2.05vw",
-    },
-    mainWindow: {
+    root: {
       height: "100%",
       width: "100%",
-      background: "#FEFEFE",
+      position: "relative",
+      alignContent: "flex-start",
+      alignItems: "flex-start",
+
+      flexDirection: "row",
+      justifyContent: "center",
+      display: "flex",
+    },
+    sideLogo: {
+      height: "70%",
+      justifyContent: "center",
+      display: "flex",
+      alignContent: "center",
+      position: "absolute",
+      alignItems: "flex-start",
+      left: "2%",
+      top: "2%",
+
+      "@media (max-width: 900px) and (orientation: landscape)": {
+        display: "none",
+      },
+
+      "@media screen and (max-width: 768px)": {
+        display: "none",
+      },
     },
   };
 };
