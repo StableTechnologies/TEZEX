f578fc0330db2775f7e52a10d720bc44cf7825c9
Siddharth Vaderaa <svaderaa@gmail.com>
2023-10-31 12:11:59 +0530

    WIP: Modifying input logic in slippage component

--- V2/tezex/src/components/ui/elements/inputs/slippage.tsx
+++ V2/tezex/src/components/ui/elements/inputs/slippage.tsx
@@ -3,6 +3,7 @@ import { BigNumber } from "bignumber.js";
 
 import {
   Asset,
+  Id,
   Token,
   TransactingComponent,
   TransferType,
@@ -15,6 +16,10 @@ import style from "./style";
 import useStyles from "../../../../hooks/styles";
 import { UserAmountField } from "./UserAmount";
 import { useTransaction } from "../../../../hooks/transaction";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import { useDebounce } from "usehooks-ts";
+import { cleanNumericString, isNumeric } from "../../../../functions/util";
 export interface ISlippage {
   asset: Asset;
   component: TransactingComponent;
@@ -31,8 +36,87 @@ const SlippageInput: FC<ISlippage> = (props) => {
   const [selectedId, setSelectedId] = useState("0");
   const [loading, setLoading] = useState(true);
   const [input, setInput] = useState<string>("0.5");
+  const debouncedValue = useDebounce<string>(input, 500);
+  const [isZeroOnFocus, setIsZeroOnFocus] = useState(false);
   const transactionOps = useTransaction(props.component);
+  const [id, setId] = useState<Id | undefined>(undefined);
 
+  // Set id on new transaction and set loading to true
+  useEffect(() => {
+    const transactionId = transactionOps.getActiveTransaction()?.id;
+    if (!id && transactionId) {
+      setId(transactionId);
+      setLoading(true);
+    }
+    if (id && transactionId && id !== transactionId) {
+      setId(transactionId);
+      setLoading(true);
+    }
+  }, [transactionOps.getActiveTransaction, id]);
+
+  //
+  // boolean check to see  if updates can be made
+  const canUpdate = useCallback(() => {
+    return !(transactionOps.loading || transactionOps.transacting);
+  }, [transactionOps.loading, transactionOps.transacting]);
+
+  // callback to set loading to false
+  const setLoadingFalse = useCallback(() => {
+    //check if updates can be made and set loading to false
+    canUpdate() &&
+      setLoading((loading) => {
+        //only update if loading is true
+        if (loading === false) return loading;
+        else return false;
+      });
+  }, [canUpdate]);
+
+  // callback to set input value
+  const setInputValue = useCallback((slippage: string) => {
+    //check if updates can be made and set input value
+    setInput((value) => {
+      //only update if  input is different
+      if (value === slippage) return value;
+      else return slippage;
+    });
+  }, []);
+
+  // call back to load value and set loading to false
+  const loadValue = useCallback(() => {
+    // get curent slippage of transaction in context
+    const slippage = transactionOps.getActiveTransaction()?.slippage.toFixed(2);
+    if (slippage) {
+      //update input if different
+      setInputValue(slippage);
+      //handle loading
+      setLoadingFalse();
+    }
+  }, [transactionOps.getActiveTransaction, setLoadingFalse]);
+
+  // on id change , load value and handle loading
+  useEffect(() => {
+    const transactionId = transactionOps.getActiveTransaction()?.id;
+    // Load value if loading is true and  transactionid is present
+    if (loading && transactionId) loadValue();
+  }, [transactionOps.getActiveTransaction, loading, loadValue]);
+  // track hook state for loading and transacting  and set loading to true
+  useEffect(() => {
+    if (!canUpdate()) {
+      console.log("set loading to true");
+      setLoading(true);
+    }
+  }, [canUpdate]);
+
+  // Callback to check if  old slippage differs from current transaction slippage
+  // if it does it calls an update
+  const updateAmount = useCallback(
+    async (slippage: string, oldSlippage: string) => {
+      if (slippage !== oldSlippage) {
+        canUpdate() && (await transactionOps.updateAmount(undefined, slippage));
+      }
+    },
+    [canUpdate, transactionOps.updateAmount]
+  );
   const onChange = useCallback(
     async (value: string) => {
       const transaction = transactionOps.getActiveTransaction();
@@ -57,12 +141,83 @@ const SlippageInput: FC<ISlippage> = (props) => {
       setLoading(false);
     }
   }, [props.value, props.loading]);
+
   useEffect(() => {
     if (!loading) {
       onChange(input);
     }
   }, [input]);
 
+  //calback to check if input is selected
+  const isInputSelected = useCallback(() => {
+    return selectedId === "input";
+  }, [selectedId]);
+
+  const SlippageInput: FC = () => {
+    const styles = useStyles(style, props.scalingKey);
+    const handleFocus = undefined;
+    const handleBlur = undefined;
+
+    //calback to handle slippage input change
+    const handleChange = useCallback(
+      (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        console.log("handleChange, loading", loading);
+        if (!isInputSelected() || loading) return;
+
+        const newValue = event.target.value;
+
+        if (newValue.length < input.length) {
+          setInput(newValue);
+        } else {
+          const result = cleanNumericString(newValue);
+          if (isNumeric(result)) setInputValue(result);
+        }
+      },
+      [isInputSelected, loading, input] // dependencies
+    );
+
+    return (
+      <Box sx={styles.slippageInput.box}>
+        <TextField
+          autoComplete="off"
+          autoFocus
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          disabled={!isInputSelected()}
+          onChange={handleChange}
+          value={input}
+          sx={styles.slippageInput}
+          InputProps={{
+            disableUnderline: true,
+            endAdornment: (
+              <InputAdornment
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  right: ".7vw",
+                  padding: "0px 0px 0px 0px",
+                }}
+                position="start"
+              >
+                %
+              </InputAdornment>
+            ),
+            sx: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            },
+          }}
+          inputProps={{
+            inputMode: "decimal",
+          }}
+          size="small"
+          variant="standard"
+        />
+      </Box>
+    );
+  };
   interface SlippageTabProps {
     id: string;
     label?: React.ReactNode;
