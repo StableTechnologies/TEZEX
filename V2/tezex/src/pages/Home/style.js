b08fc8e054b75528f8509bf0c20a7daefd88c782
Siddharth Vaderaa <svaderaa@gmail.com>
2024-04-26 11:56:15 +0200

    Add toggles to mobile view

--- V2/tezex/src/pages/Home/style.js
+++ V2/tezex/src/pages/Home/style.js
@@ -14,16 +14,12 @@ const style = (theme, scale = 1) => {
       mobile: {
         "&.MuiGrid2-root": {
           position: "relative",
-          marginTop: `calc(3.89vw * ${scale})`,
+          marginTop: `calc(0vw * ${scale})`,
           marginBottom: `calc(2.22vw * ${scale})`,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           padding: "0px 0px 0px 0px ",
-
-          [theme.breakpoints.down("md")]: {
-            display: "none",
-          },
         },
       },
     },
