5fe942ee0f159c8c76830d8a0d2596bcdf44144a
Kevin Mehrabi <4044328+KevinMehrabi@users.noreply.github.com>
2026-07-28 12:47:18 -0400

    Make TEZEX logo navigate home

---
+++ V2/tezex/src/components/ui/views/header/index.test.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter, Route, Routes, useLocation } from "react-router-dom";
+import { Header } from ".";
+
+jest.mock("../../../wallet/Wallet", () => ({
+  Wallet: () => <div>Wallet</div>,
+}));
+
+jest.mock("../../../nav", () => ({
+  NavApp: () => <nav>Navigation</nav>,
+}));
+
+jest.mock("../../elements/selectors/networkSelector/NetworkSelector", () => ({
+  NetworkSelector: () => <div>Network</div>,
+}));
+
+jest.mock("../../../../hooks/styles", () => () => ({
+  isMobile: false,
+  appBar: {},
+  shell: {},
+  toolbar: {},
+  container: {},
+  logoLink: {},
+  logoLarge: {},
+  networkSelector: {},
+  nav: {},
+  actions: {},
+  themeToggle: {},
+  themeIcon: {},
+  themeToggleKnob: {},
+  wallet: {},
+  menu: {},
+  menuButton: {},
+  hide: {},
+}));
+
+jest.mock("../../../../contexts/color-mode", () => ({
+  useColorMode: () => ({ mode: "light", toggleMode: jest.fn() }),
+}));
+
+const CurrentLocation = () => {
+  const location = useLocation();
+  return <div data-testid="location">{location.pathname}</div>;
+};
+
+test("the TEZEX logo navigates to the swap home route", () => {
+  render(
+    <MemoryRouter
+      initialEntries={["/home/add"]}
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <>
+              <Header openMenu={false} toggleMenu={jest.fn()} />
+              <CurrentLocation />
+            </>
+          }
+        />
+      </Routes>
+    </MemoryRouter>
+  );
+
+  fireEvent.click(screen.getByRole("link", { name: "TEZEX home" }));
+
+  expect(screen.getByTestId("location")).toHaveTextContent("/home/swap");
+});
